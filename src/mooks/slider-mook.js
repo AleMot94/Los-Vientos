8eb5065215d9538fd9cf5b0304243a1c8e60f48b
AleMot94 <Alector666@gmai.com>
2024-08-10 15:31:00 -0300

    slider walking

--- src/mooks/slider-mook.js
+++ src/mooks/slider-mook.js
@@ -3,4 +3,4 @@ import carrusel1 from "../assets/Carrusel/carrusel1.jpg"
 import carrusel2 from "../assets/Carrusel/carrusel2.jpg"
 import carrusel3 from "../assets/Carrusel/carrusel3.jpg"
 
-export const sliderImgs = [carrusel1, carrusel2, carrusel3]
+export const sliderImgs = [carrusel1, carrusel2, carrusel3, carrusel1]
