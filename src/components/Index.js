d3f1703d4eb9e737ed4b15c379425b5e73877f28
Patricio Resio <patricio.resio@gmail.com>
2024-08-23 12:15:05 -0300

    fixed assets

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,10 +1,19 @@
 import Navbar from "./Navbar/Navbar";
 import IconNav from "./IconNav/IconNav";
-import SlaiderlHome from "./SlaiderlHome/SlaiderHome";
+import SliderlHome from "./SliderlHome/SliderHome";
 import BedroomsContainer from "./Bedrooms/BedroomsContainer";
 import GaleryImgs from "./Galery/GaleryImgs";
 import Location from "./Location/Location";
 import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 
-export {Navbar, IconNav, SlaiderlHome, BedroomsContainer, GaleryImgs, Location, Contact, Footer}
+export {
+  Navbar,
+  IconNav,
+  SliderlHome,
+  BedroomsContainer,
+  GaleryImgs,
+  Location,
+  Contact,
+  Footer,
+};
--- src/mooks/bedrooms-mook.js
+++ src/mooks/bedrooms-mook.js
@@ -1,44 +1,44 @@
-import sonqoyBanio from "../assets/Bedrooms/banio-sonqoy.jpg";
-import sonqoyBanio1 from "../assets/Bedrooms/banio1-sonqoy.jpg";
-import sonqoyCamas from "../assets/Bedrooms/camas-sonqoy.jpg";
-import sonqoyEntrada from "../assets/Bedrooms/entrada-sonqoy.jpg";
-import sonqoyPortada from "../assets/Bedrooms/portada-sonqoy.jpg";
+import sonqoyBanio from "../assets/Bedrooms/1-Sonqoy/banio-sonqoy.jpg";
+import sonqoyBanio1 from "../assets/Bedrooms/1-Sonqoy/banio1-sonqoy.jpg";
+import sonqoyCamas from "../assets/Bedrooms/1-Sonqoy/camas-sonqoy.jpg";
+import sonqoyEntrada from "../assets/Bedrooms/1-Sonqoy/entrada-sonqoy.jpg";
+import sonqoyPortada from "../assets/Bedrooms/1-Sonqoy/portada-sonqoy.jpg";
 
-import elFuegoBanio from "../assets/Bedrooms/banio-el-fuego.jpg";
-import elFuegoBanio1 from "../assets/Bedrooms/banio1-el-fuego.jpg";
-import elFuegoCamas from "../assets/Bedrooms/camas-el-fuego.jpg";
-import elFuegoEntrada from "../assets/Bedrooms/entrada-el-fuego.jpg";
-import elFuegoPortada from "../assets/Bedrooms/portada-el-fuego.jpg";
+import elFuegoBanio from "../assets/Bedrooms/2-El-Fuego/banio-el-fuego.jpg";
+import elFuegoBanio1 from "../assets/Bedrooms/2-El-Fuego/banio1-el-fuego.jpg";
+import elFuegoCamas from "../assets/Bedrooms/2-El-Fuego/camas-el-fuego.jpg";
+import elFuegoEntrada from "../assets/Bedrooms/2-El-Fuego/entrada-el-fuego.jpg";
+import elFuegoPortada from "../assets/Bedrooms/2-El-Fuego/portada-el-fuego.jpg";
 
-import losBuhosBanio from "../assets/Bedrooms/banio-los-buhos.jpg";
-import losBuhosCamas1 from "../assets/Bedrooms/camas1-los-buhos.jpg";
-import losBuhosCamas from "../assets/Bedrooms/camas-los-buhos.jpg";
-import losBuhosEntrada from "../assets/Bedrooms/entrada-los-buhos.jpg";
-import losBuhosPortada from "../assets/Bedrooms/portada-los-buhos.jpg";
+import losBuhosBanio from "../assets/Bedrooms/3-Los-Buhos/banio-los-buhos.jpg";
+import losBuhosCamas1 from "../assets/Bedrooms/3-Los-Buhos/camas1-los-buhos.jpg";
+import losBuhosCamas from "../assets/Bedrooms/3-Los-Buhos/camas-los-buhos.jpg";
+import losBuhosEntrada from "../assets/Bedrooms/3-Los-Buhos/entrada-los-buhos.jpg";
+import losBuhosPortada from "../assets/Bedrooms/3-Los-Buhos/portada-los-buhos.jpg";
 
-import elAmanecerBanio from "../assets/Bedrooms/banio-el-amanecer.jpg";
-import elAmanecerCamas1 from "../assets/Bedrooms/camas1-el-amanecer.jpg";
-import elAmanecerCamas from "../assets/Bedrooms/camas-el-amanecer.jpg";
-import elAmanecerEntrada from "../assets/Bedrooms/entrada-el-amanecer.jpg";
-import elAmanecerPortada from "../assets/Bedrooms/portada-el-amanecer.jpg";
+import elAmanecerBanio from "../assets/Bedrooms/4-El-Amanecer/banio-el-amanecer.jpg";
+import elAmanecerCamas1 from "../assets/Bedrooms/4-El-Amanecer/camas1-el-amanecer.jpg";
+import elAmanecerCamas from "../assets/Bedrooms/4-El-Amanecer/camas-el-amanecer.jpg";
+import elAmanecerEntrada from "../assets/Bedrooms/4-El-Amanecer/entrada-el-amanecer.jpg";
+import elAmanecerPortada from "../assets/Bedrooms/4-El-Amanecer/portada-el-amanecer.jpg";
 
-import losTerosBanio from "../assets/Bedrooms/banio-los-teros.jpg";
-import losTerosBanio1 from "../assets/Bedrooms/banio1-los-teros.jpg";
-import losTerosCamas from "../assets/Bedrooms/camas-los-teros.jpg";
-import losTerosCamas1 from "../assets/Bedrooms/camas1-los-teros.jpg";
-import losTerosPortada from "../assets/Bedrooms/portada-los-teros.jpg";
+import losTerosBanio from "../assets/Bedrooms/5-Los-Teros/banio-los-teros.jpg";
+import losTerosBanio1 from "../assets/Bedrooms/5-Los-Teros/banio1-los-teros.jpg";
+import losTerosCamas from "../assets/Bedrooms/5-Los-Teros/camas-los-teros.jpg";
+import losTerosCamas1 from "../assets/Bedrooms/5-Los-Teros/camas1-los-teros.jpg";
+import losTerosPortada from "../assets/Bedrooms/5-Los-Teros/portada-los-teros.jpg";
 
-import elAtardecerBanio from "../assets/Bedrooms/banio-el-atardecer.jpg";
-import elAtardecerCamas1 from "../assets/Bedrooms/camas1-el-atardecer.jpg";
-import elAtardecerCamas from "../assets/Bedrooms/camas-el-atardecer.jpg";
-import elAtardecerEntrada from "../assets/Bedrooms/entrada-el-atardecer.jpg";
-import elAtardecerPortada from "../assets/Bedrooms/portada-el-atardecer.jpg";
+import elAtardecerBanio from "../assets/Bedrooms/6-El-Atardecer/banio-el-atardecer.jpg";
+import elAtardecerCamas1 from "../assets/Bedrooms/6-El-Atardecer/camas1-el-atardecer.jpg";
+import elAtardecerCamas from "../assets/Bedrooms/6-El-Atardecer/camas-el-atardecer.jpg";
+import elAtardecerEntrada from "../assets/Bedrooms/6-El-Atardecer/entrada-el-atardecer.jpg";
+import elAtardecerPortada from "../assets/Bedrooms/6-El-Atardecer/portada-el-atardecer.jpg";
 
-import huayraHuasiBanio from "../assets/Bedrooms/banio-huayra-huasi.jpg";
-import huayraHuasiCamas1 from "../assets/Bedrooms/camas1-huayra-huasi.jpg";
-import huayraHuasiCamas from "../assets/Bedrooms/camas-huayra-huasi.jpg";
-import huayraHuasiPasillo from "../assets/Bedrooms/pasillo-huayra-huasi.jpg";
-import huayraHuasiPortada from "../assets/Bedrooms/portada-huayra-huasi.jpg";
+import huayraHuasiBanio from "../assets/Bedrooms/7-Huayra-Huasi/banio-huayra-huasi.jpg";
+import huayraHuasiCamas1 from "../assets/Bedrooms/7-Huayra-Huasi/camas1-huayra-huasi.jpg";
+import huayraHuasiCamas from "../assets/Bedrooms/7-Huayra-Huasi/camas-huayra-huasi.jpg";
+import huayraHuasiPasillo from "../assets/Bedrooms/7-Huayra-Huasi/pasillo-huayra-huasi.jpg";
+import huayraHuasiPortada from "../assets/Bedrooms/7-Huayra-Huasi/portada-huayra-huasi.jpg";
 
 export const bedrooms = [
   {
