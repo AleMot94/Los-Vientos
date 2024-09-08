277085e083e3065c1fae0aa0310e7c34701acd19
AleMot94 <Alector666@gmai.com>
2024-09-08 01:40:15 -0300

    filter galery imgs working

--- src/components/Galery/GaleryImgs.jsx
+++ src/components/Galery/GaleryImgs.jsx
@@ -2,31 +2,43 @@ import { bedrooms } from "../../mooks/bedrooms-mook"
 import { arrayAreaComun } from "../../mooks/area-comun-mock"
 import { patioArray } from "../../mooks/patio-mock"
 import { arrayPiscina } from "../../mooks/piscina-mock"
-
-/* const buttonsArray = ["todo", "habitaciones", "area-comun", "patio", "pileta"] */
+import { useState } from "react"
 
 const arrayBedrooms = bedrooms.flatMap((bedroom) => bedroom.imgs)
-
 const imgsArray = [...arrayAreaComun, ...patioArray, ...arrayPiscina, ...arrayBedrooms]
-
 const buttonsArray = ["todo", ...new Set(imgsArray.map((img) => img.category))]
 
 const GaleryImgs = () => {
+  
+  const [categorys] = useState(buttonsArray)
+  const [imgs, setImgs] = useState(imgsArray)
+  const [allImgs] = useState(imgsArray)
+
+  const filterCategory = (category) => {
+		if (category === 'todo'){
+			setImgs(allImgs)
+			return
+		}
+
+    const filteredData = allImgs.filter(img => img.category === category);
+		setImgs(filteredData)
+	}
+  
   return (
     <section className="galery-conteiner">
       <p>GALERIA</p>
       <div className="buttons-container-galery">
         <ul className="buttons-filter-galery">
-          {buttonsArray.map((button) => {
+          {categorys.map((button) => {
             return (
-              <button key={button}>{button}</button>
+              <button type='button' key={button} onClick={() => filterCategory(button)}>{button}</button>
             )
           })}
         </ul>
       </div>
 
       <div className="galery-imgs">
-        {imgsArray.map((img) => {
+        {imgs.map((img) => {
           return (
             <img key={img.id} src={img.img} alt={img.name + "foto"} />
           )
